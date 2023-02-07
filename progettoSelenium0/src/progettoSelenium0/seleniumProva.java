52b6574bf7478adaab6312b13dec985616e2daaa
sidibe989 <saliousidibe69.sidibe@gmail.com>
2023-02-07 17:25:36 +0100

    ho fatto la modifica richesta!

---
+++ progettoSelenium0/src/progettoSelenium0/seleniumProva.java
@@ -0,0 +1,22 @@
+package progettoSelenium0;
+
+public class seleniumProva {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Unmero1=0;
+		int Unmero2=0;
+		double Somma =0;
+		
+		Somma= Unmero1 + Unmero2;
+		if(Somma !=0) {
+			System.out.print(Somma);
+			
+		}
+		else {
+			System.out.print("Fai attenzione richiesta non valido!");
+		}
+
+	}
+
+}
